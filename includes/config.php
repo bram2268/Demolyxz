294e14b70a56a2eeb2f8de1a1c5ce7f4e0dcb3ae
bram2268 <bram2268@gmail.com>
2018-03-02 20:33:13 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,31 @@
 <?php
   $mydomain = $_SERVER['HTTP_HOST'];
   $domaintoproxy = "https://thepiratebay.org";
-
+<!-- PopAds.net Popunder Code for www.tpbay.win -->
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+  var _pop = _pop || [];
+  _pop.push(['siteId', 2434261]);
+  _pop.push(['minBid', 0]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 0]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', true]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0]; 
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
+/*]]>/* */
+</script>
+<!-- PopAds.net Popunder Code End -->
   # Add your custom code here and it will be inserted before the closing </body> tag (e.g. Google Analytics)
   $footerCode = <<<EOT
 <!-- <a href="https://proxybay.bz">More Pirate Bay Proxies</a><br> -->
